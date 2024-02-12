d63e9ae978fa73984377f1f656149ca9052c2594
Umut-2647 <23010903072@subu.edu.tr>
2024-02-12 23:17:27 +0300

    10.Directory

---
+++ 10Hough_Transforms/hough_circle_transforms.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+coins=cv2.imread("C:\\Users\\umuty\\Downloads\\coins.jpg")
+balls=cv2.imread("C:\\Users\\umuty\\Downloads\\balls.jpg")
+
+gray_c=cv2.cvtColor(coins,cv2.COLOR_BGR2GRAY)
+gray_b=cv2.cvtColor(balls,cv2.COLOR_BGR2GRAY)
+
+coins_blur=cv2.medianBlur(gray_c,5)  ##resimleri blurluyoruz
+balls_blur=cv2.medianBlur(gray_b,5)
+
+
+
+circles=cv2.HoughCircles(gray_c,cv2.HOUGH_GRADIENT,1,coins.shape[0]/4,param1=200,param2=10,minRadius=60,maxRadius=90)
+
+
+
+##bunun içine girilen değerler oldukça önemlidir
+
+print(circles)  ##içinde çıkan değerler küsüratlı değerlerdir
+
+
+if circles is not None:
+    circles=np.uint16(np.around(circles))  ##circlesin içinde tuttuğu değerleri yuvarlayıp tekrar circles değişkenine atıyoruz
+    for i in circles[0,:]:
+        cv2.circle(coins,(i[0],i[1]),i[2],(0,255,0),3)
+
+cv2.imshow("Coins",coins)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
---
+++ 10Hough_Transforms/hough_line_transforms_video.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture("C:\\Users\\umuty\\Downloads\\line.mp4")
+
+while True:
+    ret,frame=cap.read()
+    frame=cv2.resize(frame,(640,480))
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    lower_yellow=np.array([20,100,100],np.uint8) ##burda videonun sarı kısmını ayırıyoruz
+    upper_yellow=np.array([30,255,255],np.uint8)
+
+    mask=cv2.inRange(hsv,lower_yellow,upper_yellow)
+
+    edges=cv2.Canny(mask,75,250) ##mask uyguladığımız videonun kenarlarını buluyoruz
+
+    lines=cv2.HoughLinesP(edges,1,np.pi/180,50,maxLineGap=50)  ##burda ise birleştireceğimiz çizgileri buluyoruz
+
+
+    for line in lines:  ##burda for dongüsüyle koordinatlardan birleştirme yapacağız
+        (x1,y1,x2,y2)=line[0]
+        cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),5)
+
+    cv2.imshow("frame",frame)
+
+    if (cv2.waitKey(20) & 0XFF==ord('q')):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
