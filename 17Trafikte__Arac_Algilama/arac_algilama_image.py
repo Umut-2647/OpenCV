4aa5ae2a3717b0d4c93993cbf97a7cb17b79bc9b
Umut-2647 <23010903072@subu.edu.tr>
2024-02-23 16:44:45 +0300

    Trafikte Arac Algilama

---
+++ 17Trafikte__Arac_Algilama/arac_algilama_image.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img=cv2.imread("C:\\Users\\umuty\\Desktop\\OpenCV\\test_images\\car.jpg")
+
+##cascade dosyaları her zaman duzgun calısmayabilir
+car_cascade=cv2.CascadeClassifier("C:\\Users\\umuty\\Desktop\\OpenCV\\haarCascade\\car.xml")
+
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #resmi gri tonlara ceviriyorz
+
+cars=car_cascade.detectMultiScale(gray,1.1,1)
+
+##bu bodies degiskeni 4 deger tutar. Sol ust kosenin koordinatları ve genislik ve yukseklik
+
+
+for (x,y,w,h) in cars:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("Img",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 17Trafikte__Arac_Algilama/arac_algilama_video.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture("C:\\Users\\umuty\\Desktop\\OpenCV\\test_videos\\car.mp4")
+
+##cascade dosyaları her zaman duzgun calısmayabilir
+
+car_cascade=cv2.CascadeClassifier("C:\\Users\\umuty\\Desktop\\OpenCV\\haarCascade\\car.xml")
+
+while 1:
+    ret,frame=cap.read()
+    frame=cv2.resize(frame,(640,360))
+    if ret==False:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # resmi gri tonlara ceviriyorz
+    cars = car_cascade.detectMultiScale(gray, 1.2, 2)
+
+    ##bu bodies degiskeni 4 deger tutar. Sol ust kosenin koordinatları ve genislik ve yukseklik
+
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Frame",frame)
+
+    if(cv2.waitKey(20)& 0xFF==ord("q")):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
