05b113850377f91018642c3490ad71f15f5c463f
Umut-2647 <23010903072@subu.edu.tr>
2024-02-23 15:42:48 +0300

    Maske alistirmasi

--- alistirma_body_mask/body_mask.py
+++ alistirma_body_mask/body_mask.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 cap=cv2.VideoCapture(0,cv2.CAP_DSHOW)
-def nothing(x):
+def nothing(x):  #trackbar kullanılacagı icin bos bir fonksiyona ihtiyacimiz var
     pass
 
 cv2.namedWindow("Trackbar")
@@ -12,6 +12,8 @@ cv2.resizeWindow("Trackbar",500,500)
 cv2.createTrackbar("Lower - H","Trackbar",0,180,nothing)
 cv2.createTrackbar("Lower - S","Trackbar",0,255,nothing)
 cv2.createTrackbar("Lower - V","Trackbar",0,255,nothing)
+
+##trackbarları olusturuyırz
 
 
 cv2.createTrackbar("Upper - H","Trackbar",0,180,nothing)
@@ -32,6 +34,7 @@ while True:
     lower_h=cv2.getTrackbarPos("Lower - H","Trackbar")
     lower_s=cv2.getTrackbarPos("Lower - S","Trackbar")
     lower_v=cv2.getTrackbarPos("Lower - V","Trackbar")
+#trackbarları cagırıyoruz
 
     upper_h = cv2.getTrackbarPos("Upper - H", "Trackbar")
     upper_s = cv2.getTrackbarPos("Upper - S", "Trackbar")
@@ -40,7 +43,7 @@ while True:
     lower_color= np.array([lower_h,lower_s,lower_v])
     upper_color=np.array([upper_h,upper_s,upper_v])
 
-    mask=cv2.inRange(frame_hsv,lower_color,upper_color)
+    mask=cv2.inRange(frame_hsv,lower_color,upper_color)  #maske uyguluyor
 
     cv2.imshow("Original",frame)
     cv2.imshow("Mask",mask)
