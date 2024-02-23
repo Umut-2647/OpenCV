fd2e52d052b17244b439df8f90127f695a285160
Umut-2647 <23010903072@subu.edu.tr>
2024-02-23 16:11:30 +0300

    Goz Algilama

---
+++ 15Eye_Detection/eye_detection_image.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+img=cv2.imread("C:\\Users\\umuty\\Desktop\\OpenCV\\test_images\\eye.png")
+
+##mantık su sekilde ilk once yuz tespit edilcek yuz tespit edildikten sonra gozler aranacak bu yuzden her iki haar cascade de eklenecek
+
+face_cascade=cv2.CascadeClassifier("C:\\Users\\umuty\\Desktop\\OpenCV\\haarCascade\\frontalface.xml")
+#bu fonksiyon sayesinde yuz algılama cascade dosyasını calısmamiza dahil ediyoruz
+
+eye_cascade=cv2.CascadeClassifier("C:\\Users\\umuty\\Desktop\\OpenCV\\haarCascade\\eye.xml")
+#bu fonksiyon sayesinde goz algılama cascade dosyasını calısmamiza dahil ediyoruz
+
+
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #resmi gri tonlara ceviriyorz
+
+faces=face_cascade.detectMultiScale(gray,1.3,7)   #bu fonksiyon cascade dosyasını kullanarak resim uzerindeki yuzleri bulup koordinatlari degiskene atıyor
+
+##bu faces degiskeni 4 deger tutar. Sol ust kosenin koordinatları ve genislik ve yukseklik
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)  ##burda bulunan yuze bir dikdortgen ciziyoruyuz
+
+img2=img[y:y+h,x:x+w]       #yukarda dikdortgen cizdigimiz degerleri yaziyoruz       #yuzlerin yakalandıgı bolgeyi tutacak
+gray2=gray[y:y+h,x:x+w]
+
+eyes=eye_cascade.detectMultiScale(gray2)
+
+for (ex,ey,ew,eh) in eyes:
+    cv2.rectangle(img2,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow("image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 15Eye_Detection/eye_detection_webcam.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+
+# 2. Webcam'den veya harici bir kameradan görüntü almak için 0,1 gibi değerler yazarız. Webcam için bu değer '0' dır.
+
+cap=cv2.VideoCapture(0)
+
+# 3. Kullanacağımız cascade dosyasını çalışmamıza dahil edelim.
+
+face_cascade=cv2.CascadeClassifier("C:\\Users\\umuty\\Desktop\\OpenCV\\haarCascade\\frontalface.xml")  #bu fonksiyon sayesinde cascade dosyasını calısmamiza dahil ediyoruz
+eye_cascade=cv2.CascadeClassifier("C:\\Users\\umuty\\Desktop\\OpenCV\\haarCascade\\eye.xml")
+#4. Sonsuz bir döngü ile her kareyi(frame) tek tek inceleyelim.
+
+
+while 1:
+    ret,frame=cap.read() #iki deger dondurur biri dogru okuduysa 1 degerini yanlıs okuduysa 0 degerini digeride frameleri
+    if ret==False:
+        break
+    frame=cv2.resize(frame,(640,480))
+    frame=cv2.flip(frame,1)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)  #resimdeki aydınlıgın ve karanlıgın daha rahat tespiti icin resmi gri tonlara ceiviriyoruz
+
+    faces=face_cascade.detectMultiScale(gray,1.1,7) #bu fonksiyon cascade dosyasını kullanarak resim uzerindeki yuzleri bulup koordinatlari degiskene atıyor
+    ##bu faces degiskeni 4 deger tutar. Sol ust kosenin koordinatları ve genislik ve yukseklik
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)            ##burda bulunan yuze bir dikdortgen ciziyoruyuz
+
+    roi_frame = frame[y:y + h, x:x + w] #yukarda dikdortgen cizdigimiz degerleri yaziyoruz       #yuzlerin yakalandıgı bolgeyi tutacak
+
+    roi_gray= gray[y:y + h, x:x + w]
+
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_frame,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
+
+    cv2.imshow("Frame",frame)
+
+    if cv2.waitKey(5)& 0xFF==ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
