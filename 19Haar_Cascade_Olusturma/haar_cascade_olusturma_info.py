97d909b3c2b17ddb07905e4c8fec506302f4e24a
Umut-2647 <23010903072@subu.edu.tr>
2024-03-01 15:43:48 +0300

    Kendi Haar cascademizi olusturma

---
+++ 19Haar_Cascade_Olusturma/haar_cascade_olusturma_info.py
@@ -0,0 +1,20 @@
+##kendi haar cascademizi olustururken izleyeceğimiz adımlar
+
+#Haar egitici yonetiminin belirlenmesi
+
+#-1Haar cascade trainer uygulamasını kurmak
+
+#-2Nesnenin belirlenmesi
+
+#-3Veri Kumesi   biz hazır veri kumeleri kullanacaz
+
+#4- Uygulama
+
+
+
+#veri kumesi olustururken negative ve positive resimler yuklenecek
+
+#Negatif resimler=> aradığımız resmin olmadığı resimler yuklencek
+
+#Positive Resimler=> aradıgımız resmin bulundugu resimler yuklenecek
+
